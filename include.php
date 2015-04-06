0dcd988317ebc09ffbae928ac0a9a1f5aeac371f
Nik Samokhvalov <nik@samokhvalov.info>
2015-04-06 21:47:28 +0300

    Classes of basis components, helpers and traits

--- include.php
+++ include.php
@@ -1,2 +1,9 @@
 <?php
 
+\Bitrix\Main\Loader::registerAutoLoadClasses('bex.bbc', [
+    'Bex\Bbc\Basis' => 'lib/basis.php',
+    'Bex\Bbc\BasisRouter' => 'lib/basisrouter.php',
+    'Bex\Bbc\Traits\Common' => 'lib/traits/common.php',
+    'Bex\Bbc\Traits\Elements' => 'lib/traits/elements.php',
+    'Bex\Bbc\Helpers\ComponentParameters' => 'lib/helpers/componentparameters.php',
+]);
---
+++ install/version.php
@@ -0,0 +1,6 @@
+<?php
+
+$arModuleVersion = array(
+    'VERSION' 		=> '0.0.1',
+    'VERSION_DATE' 	=> '2015-04-06 21:45:00'
+);
