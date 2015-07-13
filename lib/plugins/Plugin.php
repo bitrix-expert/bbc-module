a531b680e8f05b14b9efe1f49ad41781d5347a1e
Nik Samokhvalov <nik@samokhvalov.info>
2015-07-13 22:53:06 +0300

    Moved Advanced component to BBC

---
+++ lib/plugins/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Bex\Bbc\Plugins;
+
+use Bitrix\Main\ArgumentTypeException;
+
+abstract class Plugin
+{
+    /**
+     * @var static
+     */
+    protected static $instance = [];
+
+    /**
+     * @var \CBitrixComponent
+     */
+    protected $component;
+
+    protected function __construct($component)
+    {
+        $this->component = $component;
+    }
+
+    /**
+     * @return static
+     *
+     * @throws ArgumentTypeException
+     */
+    public static function getInstance()
+    {
+        if (!isset(static::$instance[get_called_class()]))
+        {
+            $component = func_get_arg(0);
+
+            if (!$component instanceof \CBitrixComponent)
+            {
+                throw new ArgumentTypeException('$component', '\CBitrixComponent');
+            }
+
+            static::$instance[get_called_class()] = new static($component);
+        }
+
+        return static::$instance[get_called_class()];
+    }
+
+    public static function getClass()
+    {
+        return get_called_class();
+    }
+
+    public function dependencies()
+    {
+
+    }
+}
---
+++ lib/plugins/PluginManager.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @link http://bbc.bitrix.expert
+ * @copyright Copyright © 2015 Nik Samokhvalov
+ * @license MIT
+ */
+
+namespace Bex\Bbc\Plugins;
+
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\ArgumentTypeException;
+
+/**
+ * @author Nik Samokhvalov <nik@samokhvalov.info>
+ */
+class PluginManager
+{
+    /**
+     * @var Plugin
+     */
+    private $pluginsInstance;
+    protected $component;
+
+    /**
+     * @param \CBitrixComponent|AdvancedComponentTrait $component
+     *
+     * @throws ArgumentTypeException
+     */
+    public function __construct($component)
+    {
+        $this->component = $component;
+        $component->configurate();
+    }
+
+    public function trigger($action)
+    {
+        if (!empty($this->pluginsInstance))
+        {
+            foreach ($this->pluginsInstance as $instance)
+            {
+                if (method_exists($instance, $action))
+                {
+                    call_user_func($instance->$action());
+                }
+            }
+        }
+    }
+
+    public function add($plugin)
+    {
+        if (is_array($plugin) && isset($plugin['class']))
+        {
+            $class = $plugin['class'];
+        }
+        elseif (is_string($plugin) && strlen($plugin) > 0)
+        {
+            $class = $plugin;
+        }
+        else
+        {
+            throw new ArgumentException('invalid plugin', $plugin);
+        }
+
+        $pluginInstance = call_user_func_array([$class, 'getInstance'], [$this->component]);
+
+        if ($pluginInstance instanceof Plugin)
+        {
+            $this->pluginsInstance[$class] = $pluginInstance;
+        }
+        else
+        {
+            throw new \InvalidArgumentException('Plugin not instanceof Plugin');
+        }
+    }
+
+    public function remove($class)
+    {
+        unset($this->pluginsInstance[$class]);
+    }
+
+    public function get($class)
+    {
+        return $this->pluginsInstance[$class];
+    }
+}
